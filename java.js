fcf0b94e97121f927f5b07b12291e2ecf4b08b35
titi <raymathild@gmail.com>
2018-06-28 05:06:14 +0200

    ajout bouton mode psychédélique change couleur background

--- java.js
+++ java.js
@@ -1,6 +1,26 @@
 //javascript pour bettermotherfuckingwebsite
 
 
-setTimeout(function(){
-	confirm('Hey le site xxxvidsxxx est trop bien. Viens dessus stp please');
-}, 10000);
+// setTimeout(function(){
+// 	confirm('Hey le site xxxvidsxxx est trop bien. Viens dessus stp please');
+// }, 10000);
+
+var myButton = document.querySelector('button');
+
+
+myButton.onclick = function (){
+		document.querySelector('body').style.background = "magenta",
+		document.querySelector('button').innerHTML = 'Mode normal';
+		document.querySelector('h1').style.color = 'green';
+		document.querySelector('h2').style.color = 'green';
+		document.querySelector('h3').style.color = 'green';
+
+myButton.onclick = function () {
+		document.querySelector('body').style.background = "rgb(250, 250, 250";
+		document.querySelector('button').innerHTML = 'Mode psychédélique';
+		document.querySelector('h1').style.color = 'rgb(55, 55, 55)';
+		document.querySelector('h2').style.color = 'rgb(55, 55, 55)';
+		document.querySelector('h3').style.color = 'rgb(55, 55, 55)';
+
+	} 
+} 
